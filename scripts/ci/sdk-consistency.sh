f049403e344beec1b45fc879d5b28705f48dae7e
Casey Waldren <cwaldren@launchdarkly.com>
2024-03-06 15:27:18 -0800

    again

--- scripts/ci/sdk-consistency.sh
+++ scripts/ci/sdk-consistency.sh
@@ -2,7 +2,7 @@
 
 # Checks that for each SDK ID defined in sdks.json, all other files have an entry for that ID.
 # If not, outputs a diff containing the missing IDs and exits with a non-zero status.
-#
+
 cleanup() {
   rm ./data/*.json.keys
 }
