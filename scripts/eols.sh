4f06a24aacfd505649beb03b6982d30c9fda3d76
Casey Waldren <cwaldren@launchdarkly.com>
2024-04-04 15:08:24 -0700

    remove irrelevant files:

--- scripts/eols.sh
+++ scripts/eols.sh
@@ -7,21 +7,21 @@ fi
 
 sqlite3 -json "$1" "
 SELECT
-r.id,
-r.major,
-r.minor,
-r.date,
-(SELECT DATE(MIN(r2.date), '+1 year')
-FROM sdk_releases r2
-  WHERE r2.id = r.id
-  AND (r2.major > r.major OR (r2.major = r.major AND r2.minor > r.minor))
-  AND r2.date >= r.date) AS eol
-FROM sdk_releases r
-WHERE NOT EXISTS (
-SELECT 1
-FROM sdk_releases r_inner
-WHERE r_inner.id = r.id
-AND r_inner.major = r.major
-AND r_inner.minor = r.minor
-AND r_inner.date > r.date
+  r.id,
+  r.major,
+  r.minor,
+  r.date,
+  (SELECT DATE(MIN(r2.date), '+1 year')
+    FROM sdk_releases r2
+      WHERE r2.id = r.id
+        AND (r2.major > r.major OR (r2.major = r.major AND r2.minor > r.minor))
+        AND r2.date >= r.date) AS eol
+  FROM sdk_releases r
+  WHERE NOT EXISTS (
+    SELECT 1
+    FROM sdk_releases r_inner
+    WHERE r_inner.id = r.id
+      AND r_inner.major = r.major
+      AND r_inner.minor = r.minor
+      AND r_inner.date < r.date
 ) ORDER BY r.id ASC, r.major DESC, r.minor DESC;"
