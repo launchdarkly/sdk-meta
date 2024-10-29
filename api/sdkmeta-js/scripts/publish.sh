f0badd79031adb91d7fbdb06a5f2ec06607106fb
Casey Waldren <cwaldren@launchdarkly.com>
2024-10-29 16:26:11 -0700

    use npm --dry-run

--- api/sdkmeta-js/scripts/publish.sh
+++ api/sdkmeta-js/scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if $LD_RELEASE_IS_DRYRUN ; then
-  # Dry run just pack the workspace.
   echo "Doing a dry run of publishing."
+  npm publish --dry-run --provenance --access public || { echo "npm publish --dry-run failed" >&2; exit 1; }
 else
   if $LD_RELEASE_IS_PRERELEASE ; then
     echo "Publishing with prerelease tag."
