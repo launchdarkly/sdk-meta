cc3f7a5dbcd7671af5022a20d1e56305daf32133
Casey Waldren <cwaldren@launchdarkly.com>
2024-10-29 15:54:14 -0700

    don't use yarn

--- api/sdkmeta-js/scripts/publish.sh
+++ api/sdkmeta-js/scripts/publish.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-yarn pack
 if $LD_RELEASE_IS_DRYRUN ; then
   # Dry run just pack the workspace.
   echo "Doing a dry run of publishing."
 else
   if $LD_RELEASE_IS_PRERELEASE ; then
     echo "Publishing with prerelease tag."
-    npm publish --tag prerelease --provenance --access public package.tgz || { echo "npm publish failed" >&2; exit 1; }
+    npm publish --tag prerelease --provenance --access public || { echo "npm publish failed" >&2; exit 1; }
   else
-    npm publish --provenance --access public package.tgz || { echo "npm publish failed" >&2; exit 1; }
+    npm publish --provenance --access public || { echo "npm publish failed" >&2; exit 1; }
   fi
 fi
